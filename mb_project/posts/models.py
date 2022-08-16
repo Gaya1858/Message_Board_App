ff2161ec4ff2bdbb55cba6452e7881bb37d3b04d
Gaya1858 <gayathri1858@yahoo.com>
2022-08-16 14:32:12 -0400

    simple message app that shows messages

---
+++ mb_project/posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    text = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.text[:50]
